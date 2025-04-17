82bfadec934ce126a937dcb6c61d6cb769b849fa
d4yr41n <d4yr41n@gmail.com>
2025-04-17 21:02:58 +0600

    update

--- main.py
+++ main.py
@@ -8,19 +8,25 @@ from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
 
-class Index(HTTPEndpoint):
-    async def get(self, request: Request):
-        return FileResponse("index.html")
+async def index(self, request: Request):
+    return FileResponse("index.html")
 
-    async def post(self, request: Request):
-        data = await request.json()
-        print(data)
-        return Response() 
+
+async def post(self, request: Request):
+    letter = request.path_params["letter"]
+    if letter not in "ony":
+        return Response(status_code=404)
+
+    data = await request.json()
+    with open("db/" + letter, "a") as file:
+        file.write(''.join(map(str, data)) + '\n')
+    return Response()
 
 
 app = Starlette(
     routes=[
-        Route('/', Index),
+        Route('/', index),
+        Route("/{letter}", post),
         Mount("/static", StaticFiles(directory="static"))
     ],
     # middleware=[
--- static/main.js
+++ static/main.js
@@ -1,7 +1,8 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 const rect = canvas.getBoundingClientRect()
-const button = document.querySelector("button")
+const button = document.getElementById("push")
+// const screen = document.getElementById("screen")
 
 ctx.fillStyle = "#0f0f0f"
 ctx.fillRect(0, 0, 320, 320)
@@ -19,14 +20,31 @@ let data = create_data()
 
 canvas.addEventListener("mousedown", () => state = true)
 canvas.addEventListener("mouseup", () => state = false)
-canvas.addEventListener("mousemove", e => {
-  if (state)
+canvas.addEventListener("touchstart", () => state = true)
+canvas.addEventListener("touchend", () => state = false)
+
+const draw = e => {
+  if (state) {
     x = e.x - rect.left
     y = e.y - rect.top
     x = Math.floor(x / 10) * 10
     y = Math.floor(y / 10) * 10
     ctx.fillRect(x, y, 10, 10)
+  }
+}
+
+canvas.addEventListener("touchmove", e => {
+  if (state) {
+    e = e.touches[0]
+    x = e.clientX - rect.left
+    y = e.clientY - rect.top
+    x = Math.floor(x / 10) * 10
+    y = Math.floor(y / 10) * 10
+    ctx.fillRect(x, y, 10, 10)
+  }
 })
+
+canvas.addEventListener("mousemove", draw)
 
 button.onclick = () => {
   fetch("http://127.0.0.1:8000", {
@@ -34,4 +52,6 @@ button.onclick = () => {
     body: JSON.stringify(data)
   })
 }
+
+// screen.onclick = () => document.querySelector("main").requestFullscreen()
 
